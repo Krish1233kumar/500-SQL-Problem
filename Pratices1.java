e391d3fef92499b118587210131d2c48d56a13ad
Krishna Kumar <147659839+Krish1233kumar@users.noreply.github.com>
2024-12-01 19:38:01 +0530

    Add files via upload

---
+++ Pratices1.java
@@ -0,0 +1,9 @@
+package Pratices1;
+
+public class Vehcile {
+	public void Vehcile_Details(String Vehcile_Name, int Vehcile_Prices) {
+		
+		System.out.println("Vehcile_Name is "+ Vehcile_Name+" Vehcile_Prices "+ Vehcile_Prices );
+	}
+
+}
